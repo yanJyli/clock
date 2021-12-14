26d0a292de72f43ddbaf11c2ed7475b64bc77178
yanJyli <85889195+yanJyli@users.noreply.github.com>
2021-12-14 09:08:28 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let clock = document.getElementById('clock');
+let color = document.getElementById('color');
+
+
+function hexoClock() {
+    let time = new Date();
+    let h = (time.getHours() % 12).toString();
+    let m = time.getMinutes().toString();
+    let s = time.getSeconds().toString();
+
+    if (h.length < 2) {
+        h = '0' + h;
+    }
+    if (m.length < 2) {
+        m = '0' + m;
+    }
+    if (s.length < 2) {
+        s = '0' + s;
+    }
+
+    let clockString = h + ':' + m + ':' + s;
+    let colorString = '#' + h + m + s;
+
+    clock.textContent = clockString;
+    color.textContent = colorString;
+
+    document.body.style.background = colorString;
+}
+
+hexoClock();
+setInterval(hexoClock, 1000);
